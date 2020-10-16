8d3e8a0c356ae60bbcbb87be4014165d931a2a96
Sergey <chechelsk@gmail.com>
2020-10-16 16:57:37 +0300

    014 23:00

---
+++ src/js/modules/sliders.js
@@ -0,0 +1,67 @@
+const sliders = (slides, dir, prev, next) => {
+
+    let slideIndex = 1;
+    let rotate;
+
+    const items = document.querySelectorAll(slides);
+
+    function showSlides(n) {
+        if(n > items.length) {
+            slideIndex = 1;
+        }
+        if(n < 1) {
+            slideIndex = items.length;
+        }
+        items.forEach(item => {
+            item.classList.add('animated');
+            item.style.display = 'none';
+        });
+        items[slideIndex - 1].style.display = 'block';
+    }
+
+    showSlides(slideIndex);
+
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
+
+    try {
+        const prevBtn = document.querySelector(prev),
+              nextBtn = document.querySelector(next);
+        prevBtn.addEventListener('click', () => {
+            plusSlides(-1);
+            items[slideIndex - 1].classList.remove('slideInLeft');
+            items[slideIndex - 1].classList.add('slideInRight');
+        });
+        nextBtn.addEventListener('click', () => {
+            plusSlides(1);
+            items[slideIndex - 1].classList.remove('slideInRight');
+            items[slideIndex - 1].classList.add('slideInLeft');
+        });
+    } catch(e) {}
+
+    function rotateSlider() {
+        if(dir === "vertical") {
+            rotate = setInterval(() => {
+                plusSlides(1);
+                items[slideIndex - 1].classList.add('slideInDown');
+            }, 5000);
+        } else {
+            rotate = setInterval(() => {
+                plusSlides(1);
+                items[slideIndex - 1].classList.remove('slideInRight');
+                items[slideIndex - 1].classList.add('slideInLeft');
+            }, 5000);
+        }
+    }
+    rotateSlider();
+    items[0].parentNode.addEventListener('mouseenter', () => {
+        clearInterval(rotate);
+    });
+    items[0].parentNode.addEventListener('mouseleave', () => {
+        rotateSlider();
+    });
+
+};
+
+export default sliders;
